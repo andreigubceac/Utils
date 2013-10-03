561cf6554be033b4805ff9a52aa4025ed5d53a4c
andreigubceac <andrei.gubceac@me.com>
2013-10-03 18:15:26 +0100

    Added custom toggle button with button

--- Category.UIKIt/UIButton+Additions.h
+++ Category.UIKIt/UIButton+Additions.h
@@ -8,5 +8,7 @@
 #import <UIKit/UIKit.h>
 
 @interface UIButton (Additions)
-
++ (UIButton*)buttonWithType:(UIButtonType)buttonType actionBlock:(void(^)(UIButton* b, BOOL toggle))block;
++ (UIButton*)buttonWithNormalImage:(NSString*)normalImage toggleImage:(NSString*)toggleImage actionBlock:(void(^)(UIButton* b, BOOL toggle))block;
 @end
+
--- Category.UIKIt/UIButton+Additions.m
+++ Category.UIKIt/UIButton+Additions.m
@@ -7,7 +7,25 @@
 
 #import "UIButton+Additions.h"
 
+@interface AGButtonToggle : UIButton
+@property (nonatomic, copy) void(^actionBlock)(UIButton*, BOOL);
+@end
+
+@implementation AGButtonToggle
+
+#pragma mark - private
+
+- (void)toggleAction:(UIButton*)b
+{
+    b.selected = !b.selected;
+    if (self.actionBlock)
+        self.actionBlock(self,b.selected);
+}
+
+@end
+
 @interface UIButton ()<NSCopying>
+@property (nonatomic, copy) void(^actionBlock)(UIButton*, BOOL);
 @end
 
 @implementation UIButton (Additions)
@@ -25,6 +43,23 @@
     [b setContentEdgeInsets:self.contentEdgeInsets];
     [b.titleLabel setFont:self.titleLabel.font];
     return b;
+}
+
++ (UIButton*)buttonWithNormalImage:(NSString*)normalImage toggleImage:(NSString*)toggleImage actionBlock:(void(^)(UIButton*, BOOL))block
+{
+    AGButtonToggle *_b = (AGButtonToggle*)[UIButton buttonWithType:UIButtonTypeCustom actionBlock:block];
+    [_b setImage:[UIImage imageNamed:normalImage] forState:UIControlStateNormal];
+    [_b setImage:[UIImage imageNamed:toggleImage] forState:UIControlStateSelected];
+    [_b setFrame:(CGRect){CGPointZero, [_b imageForState:UIControlStateNormal].size}];
+    return _b;
+}
+
++ (UIButton*)buttonWithType:(UIButtonType)buttonType actionBlock:(void(^)(UIButton*, BOOL))block;
+{
+    AGButtonToggle *_b    = [AGButtonToggle buttonWithType:buttonType];
+    [_b addTarget:_b action:@selector(toggleAction:) forControlEvents:UIControlEventTouchUpInside];
+    [_b setActionBlock:block];
+    return _b;
 }
 
 @end
