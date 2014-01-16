b1f95ad0c3a04c83fa276fb146276917d234b772
andreigubceac <andrei.gubceac@me.com>
2014-01-16 11:41:32 +0200

    Added custom view controller.

---
+++ Custom/UIKit/AGViewController.h
@@ -0,0 +1,17 @@
+//
+//  AGViewController.h
+//
+//  Created by Andrei Gubceac on 1/16/14.
+//  Copyright (c) 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+{
+    IBOutlet UITableView *_tableView;
+}
+@property (nonatomic, readonly) UITableView *tableView;
+- (void)loadWithTableViewStyle:(UITableViewStyle)style;//to switch in tableViewcontroller mode
+- (void)updateUI;
+@end
---
+++ Custom/UIKit/AGViewController.m
@@ -0,0 +1,73 @@
+//
+//  AGViewController.m
+//
+//  Created by Andrei Gubceac on 1/16/14.
+//  Copyright (c) 2014. All rights reserved.
+//
+
+#import "AGViewController.h"
+
+@interface AGViewController ()
+
+@end
+
+@implementation AGViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (id)init
+{
+    return [self initWithNibName:NSStringFromClass([self class]) bundle:nil];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self updateUI];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - public
+
+- (void)loadWithTableViewStyle:(UITableViewStyle)style
+{
+    _tableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame] style:style];
+    [_tableView setBackgroundView:nil];
+    self.view = _tableView;
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+}
+
+- (void)updateUI
+{
+    
+}
+#pragma uitableview
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+@end
