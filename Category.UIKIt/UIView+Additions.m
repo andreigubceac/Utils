2c1af2d7ac60d7ecdb5e81b53523790af2aaf166
andreigubceac <andrei.gubceac@me.com>
2014-05-05 23:50:44 +0300

    Minor fix.

--- Category.UIKIt/UIView+Additions.m
+++ Category.UIKIt/UIView+Additions.m
@@ -6,7 +6,7 @@
 {
     if ([self respondsToSelector:@selector(setText:)])
     {
-        if ([self respondsToSelector:@selector(boundingRectWithSize:options:attributes:context:)])
+        if ([[(UILabel*)self text] respondsToSelector:@selector(boundingRectWithSize:options:attributes:context:)])
             return [[(UILabel*)self text] boundingRectWithSize:CGSizeMake(self.width, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: [(UILabel*)self font]} context:NULL].size;
         return [[(UILabel*)self text] sizeWithFont:[(UILabel*)self font] constrainedToSize:CGSizeMake(self.width, MAXFLOAT) lineBreakMode:[(UILabel*)self lineBreakMode]];
     }
