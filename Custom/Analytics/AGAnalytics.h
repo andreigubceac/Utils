2b3840b3564c171a5b0e6a9b6854491ef29b10bc
andreigubceac <andrei.gubceac@me.com>
2014-10-02 16:50:15 +0300

    Added new object Analytcs

---
+++ Custom/Analytics/AGAnalytics.h
@@ -0,0 +1,13 @@
+//
+//  AGAnalytics.h
+//  MobiFind
+//
+//  Created by Andrei Gubceac on 10/2/14.
+//  Copyright (c) 2014 MobiFind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGAnalytics : NSObject
+
+@end
---
+++ Custom/Analytics/AGAnalytics.m
@@ -0,0 +1,13 @@
+//
+//  AGAnalytics.m
+//  MobiFind
+//
+//  Created by Andrei Gubceac on 10/2/14.
+//  Copyright (c) 2014 MobiFind. All rights reserved.
+//
+
+#import "AGAnalytics.h"
+
+@implementation AGAnalytics
+
+@end
